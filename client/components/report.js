f4d8fec115fd7b3a311f6ff642c2809fbba28f70
Liubov <luba_siagaeva@mail.ru>
2021-10-26 10:47:05 +0300

    added style for table

--- client/components/report.js
+++ client/components/report.js
@@ -4,9 +4,9 @@ class Report extends Component{
     render(){
         if (this.props.show) {
             return(
-                <section className="info_block">
-                   <table>
-                       <caption>Calibration report</caption>
+                <section className="report-block">
+                    <table className="report-block__table">
+                        <caption className="report-block__table_heading">Calibration report</caption>
                         <thead>
                             <tr>
                                 <th>Data</th>
@@ -17,26 +17,26 @@ class Report extends Component{
                             </tr>
                         </thead>
                         <tbody>
-                           {this.props.data.map((item,index) => {
+                            {this.props.data.map((item,index) => {
                                 return(
                                 <tr key={index}>
-                                    <td>{item.data}</td>
+                                    <td className="report-block__table_data">{item.data}</td>
                                     <td>{
                                         item.solution.map((inner_item, inner_index)=>{
                                             return(
-                                                <p key={inner_index}>B{inner_index+1}: №{inner_item.number}: pH {inner_item.ph_value}</p>
+                                                <span key={inner_index}>B{inner_index+1}: №{inner_item.number}: pH {inner_item.ph_value}<br/></span>
                                             )
                                         })
                                     }
                                     </td>
                                     <td>{item.slope}</td>
                                     <td>{item.offset}</td>
-                                    <td>{item.user}</td>
+                                    <td className="report-block__table_user">{item.user}</td>
                                 </tr>)
-                           })}
-                       </tbody>
-                   </table>
-                 </section>
+                            })}
+                        </tbody>
+                    </table>
+                </section>
             ) 
         } else {
         return(
